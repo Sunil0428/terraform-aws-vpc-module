fee998e0bfa4b1e6070504eebfaf7da5d5eac8c5
Sunil <sunil.nunubilli@gmail.com>
2024-11-20 02:00:22 +0530

    adde db subnet grp name

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,16 @@ output "defaultvpc" {
 output "default_main_rt" {
   value=data.aws_route_table.default_vpc_rt  
 }
+
+output "public_subnet_ids"{
+  value= aws_subnet.public[*].id
+}
+output "private_subnet_ids"{
+  value= aws_subnet.private[*].id
+}
+output "database_subnet_ids"{
+  value= aws_subnet.database[*].id
+}
+output "database_subent_grp_name"{
+  value= aws_db_subnet_group.db_subnet_grp.name
+}
