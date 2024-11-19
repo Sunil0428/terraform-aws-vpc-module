4e81c50c8fca49f195b9fc55f020dd3244af1955
Sunil <sunil.nunubilli@gmail.com>
2024-11-20 02:10:50 +0530

    adde db subnet grp name

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,6 @@ output "private_subnet_ids"{
 output "database_subnet_ids"{
   value= aws_subnet.database[*].id
 }
-output "database_subent_grp_name"{
+output "database_subnet_grp_name"{
   value= aws_db_subnet_group.db_subnet_grp.name
 }
